e2ff0e79b88812f94f8589c00f91418665bdeed6
Dmitry N Arkhipov <arkhipov@alfasatcom.ru>
2021-03-16 23:11:19 +0300

    Remaining Routes

--- project/app/api/crud.py
+++ project/app/api/crud.py
@@ -23,3 +23,18 @@ async def get(summary_id: int) -> Optional[dict]:
 async def get_all() -> List:
     summaries = await TextSummary.all().values()
     return summaries
+
+
+async def delete(summary_id: int) -> int:
+    summary = await TextSummary.filter(id=summary_id).first().delete()
+    return summary
+
+
+async def put(summary_id: int, payload: SummaryPayloadSchema) -> Optional[dict]:
+    summary = await TextSummary.filter(id=summary_id).update(
+        url=payload.url, summary=payload.summary
+    )
+    if summary:
+        updated_summary = await TextSummary.filter(id=summary_id).first().values()
+        return updated_summary[0]
+    return None
--- project/app/api/summaries.py
+++ project/app/api/summaries.py
@@ -1,10 +1,15 @@
 from typing import List
 
-from fastapi import APIRouter, HTTPException, status
+from fastapi import APIRouter, HTTPException, Path, status
 
 from app.api import crud
-from app.models.pydantic import SummaryPayloadSchema, SummaryResponseSchema
 from app.models.tortoise import SummarySchema
+
+from app.models.pydantic import (  # isort:skip
+    SummaryPayloadSchema,
+    SummaryResponseSchema,
+    SummaryUpdatePayloadSchema,
+)
 
 router = APIRouter()
 
@@ -19,7 +24,7 @@ async def create_summary(payload: SummaryPayloadSchema) -> SummaryResponseSchema
 
 
 @router.get("/{summary_id}/", response_model=SummarySchema)
-async def read_summary(summary_id: int) -> SummarySchema:
+async def read_summary(summary_id: int = Path(..., gt=0)) -> SummarySchema:
     summary = await crud.get(summary_id)
     if not summary:
         raise HTTPException(
@@ -32,3 +37,25 @@ async def read_summary(summary_id: int) -> SummarySchema:
 @router.get("/", response_model=List[SummarySchema])
 async def read_all_summaries() -> List[SummarySchema]:
     return await crud.get_all()
+
+
+@router.delete("/{summary_id}/", response_model=SummaryResponseSchema)
+async def delete_summary(summary_id: int = Path(..., gt=0)) -> SummaryResponseSchema:
+    summary = await crud.get(summary_id)
+    if not summary:
+        raise HTTPException(status_code=404, detail="Summary not found")
+
+    await crud.delete(summary_id)
+
+    return summary
+
+
+@router.put("/{summary_id}/", response_model=SummarySchema)
+async def update_summary(
+    payload: SummaryUpdatePayloadSchema, summary_id: int = Path(..., gt=0)
+) -> SummarySchema:
+    summary = await crud.put(summary_id, payload)
+    if not summary:
+        raise HTTPException(status_code=404, detail="Summary not found")
+
+    return summary
